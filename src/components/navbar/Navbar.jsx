7b3b58c46788d966df5106a73f9507620cb03028
Anas Firly <anasfirly20@gmail.com>
2023-06-25 01:13:36 +0300

    adjusted

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const navItems = [
 const Navbar = () => {
   return (
     <>
-      <nav className="fixed w-full z-10 bg-[#202020]">
+      <nav className="fixed w-full z-10 bg-[#202020] z-30">
         <div className="flex justify-between px-longer2 py-shorter4 border-b-2 border-gradient-navbar">
           <div className="flex gap-1 drop-shadow-[3px_3px_4px_white]">
             <div className="border-[1rem] border-white w-0 h-0 border-t-transparent border-r-transparent" />
