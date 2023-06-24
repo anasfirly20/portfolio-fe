2dd55905e684a5d464c555bc24e5824dafac442a
Anas Firly <anasfirly20@gmail.com>
2023-06-25 00:37:09 +0300

    adjusted

---
+++ src/components/constants.js
@@ -0,0 +1,66 @@
+// Assets
+import belinsky from "../assets/Belinsky.webp";
+import turta from "../assets/Turta.webp";
+import kima from "../assets/Kima.webp";
+
+export const icons = [
+  {
+    icon: "simple-icons:javascript",
+  },
+  {
+    icon: "simple-icons:typescript",
+  },
+  {
+    icon: "simple-icons:react",
+  },
+  {
+    icon: "simple-icons:redux",
+  },
+  {
+    icon: "simple-icons:tailwindcss",
+  },
+  {
+    icon: "simple-icons:antdesign",
+  },
+  {
+    icon: "simple-icons:mui",
+  },
+  {
+    icon: "tabler:brand-framer-motion",
+  },
+  {
+    icon: "simple-icons:git",
+  },
+  {
+    icon: "simple-icons:axios",
+  },
+  {
+    icon: "simple-icons:vite",
+  },
+];
+
+export const images = [
+  {
+    name: "Belinsky Studio",
+    img: belinsky,
+    link: "https://belinskyproduction.com/",
+  },
+  {
+    name: "TurunTangan",
+    img: turta,
+    link: "https://turuntangan.id/",
+  },
+  {
+    name: "KIMA",
+    img: kima,
+    link: "https://kima.kpfu.ru/",
+  },
+];
+
+export const list = [
+  "Developed a fully fullstack responsive web app using React",
+  "Optimized key content pages for SEO objectives",
+  "Utilized Axios library to perform HTTP requests and consume RESTful APIs",
+  "Integrated Tailwind CSS for responsive component-based styling",
+  "Leveraged Redux Toolkit to implement a global state management solution",
+];
