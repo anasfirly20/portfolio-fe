85060845759de079fd8dfded94525cac7b0fa884
Anas Firly <anasfirly20@gmail.com>
2023-06-24 22:46:39 +0300

    project section progress

--- src/contents/ProjectSection.jsx
+++ src/contents/ProjectSection.jsx
@@ -3,6 +3,11 @@ import React from "react";
 // Miscellaneous
 import { Icon } from "@iconify/react";
 
+// Assets
+import belinsky from "../assets/Belinsky.webp";
+import turta from "../assets/Turta.webp";
+import kima from "../assets/Kima.webp";
+
 const ProjectSection = () => {
   const icons = [
     {
@@ -40,6 +45,21 @@ const ProjectSection = () => {
     },
   ];
 
+  const images = [
+    {
+      name: "Belinsky Studio",
+      img: belinsky,
+    },
+    {
+      name: "TurunTangan",
+      img: turta,
+    },
+    {
+      name: "KIMA",
+      img: kima,
+    },
+  ];
+
   return (
     <section className="px-longer2 py-normal">
       <h1 className="text-center text-4xl font-medium">Featured Projects</h1>
@@ -48,6 +68,16 @@ const ProjectSection = () => {
           <Icon key={i} icon={e.icon} color="#525252" className="text-4xl" />
         ))}
       </div>
+      {images.map((e, i) => (
+        <section className={`mt-20 flex ${i % 2 === 1 && "justify-end"}`}>
+          <div className="w-[430px] h-[580px] relative">
+            <img src={e.img} alt="" className="object-cover w-full h-full" />
+            <div className="bg-layer absolute inset-0 hover:opacity-0 animate500 flex justify-center items-center">
+              <h3 className="py-3 px-12 border-4 border-project">{e.name}</h3>
+            </div>
+          </div>
+        </section>
+      ))}
     </section>
   );
 };
