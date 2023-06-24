1eef0202af483ff152d8f1eba4addae483041318
Anas Firly <anasfirly20@gmail.com>
2023-06-25 00:07:46 +0300

    project section progress

--- src/contents/ProjectSection.jsx
+++ src/contents/ProjectSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ReactComponent as ArrowSvg } from "../assets/arrow.svg";
 
 // Miscellaneous
 import { Icon } from "@iconify/react";
@@ -60,6 +61,14 @@ const ProjectSection = () => {
     },
   ];
 
+  const list = [
+    "Developed a fully fullstack responsive web app using React",
+    "Optimized key content pages for SEO objectives",
+    "Utilized Axios library to perform HTTP requests and consume RESTful APIs",
+    "Integrated Tailwind CSS for responsive component-based styling",
+    "Leveraged Redux Toolkit to implement a global state management solution",
+  ];
+
   return (
     <section className="px-longer2 py-normal">
       <h1 className="text-center text-4xl font-medium">Featured Projects</h1>
@@ -70,11 +79,15 @@ const ProjectSection = () => {
       </div>
       {images.map((e, i) => (
         <section className={`mt-20 flex ${i % 2 === 1 && "justify-end"}`}>
-          <div className="w-[430px] h-[580px] relative group">
+          <div
+            className={`w-[430px] h-[580px] relative group flex ${
+              i % 2 === 1 ? "" : "flex-row-reverse"
+            }`}
+          >
             <img src={e.img} alt="" className="object-cover w-full h-full" />
             <div className="bg-layer absolute inset-0 group-hover:opacity-0 animate500 flex justify-center items-center"></div>
             <h3
-              className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] py-2 px-7 border-4 border-project animate group-hover:border-hidden ${
+              className={`animate500 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] py-2 px-7 animate border-4 border-project group-hover:border-none group-hover:opacity-0 ${
                 i % 2 === 1
                   ? "group-hover:translate-x-[-27rem]"
                   : "group-hover:translate-x-[15rem]"
@@ -82,6 +95,29 @@ const ProjectSection = () => {
             >
               {e.name}
             </h3>
+            <div
+              className={`opacity-0 group-hover:opacity-100 self-end absolute h-[60%] w-[85%] -bottom-5 border-4 border-project animate500 ${
+                i % 2 === 1
+                  ? "border-r-0 group-hover:translate-x-[-20rem]"
+                  : "border-l-0 group-hover:translate-x-[22rem]"
+              }`}
+            >
+              <div
+                className={`flex flex-col py-3 gap-3 ${
+                  i % 2 === 1 ? "px-6" : "px-12"
+                }`}
+              >
+                <div className="flex justify-between">
+                  <h3>{e.name}</h3>
+                  <ArrowSvg className="w-8 h-8" />
+                </div>
+                <ul className="space-y-2">
+                  {list.map((e) => (
+                    <li className="pSmaller3">• {e}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
         </section>
       ))}
